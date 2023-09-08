7ade8d030341be714d89c240965fef042cb63635
Cameron <cameron.obrien@selu.edu>
2023-09-08 10:15:17 -0500

    Nav Drawer
    Added Nav drawer and created 4 starting pages.

--- app/src/main/java/com/example/cubconnect/MainActivity.kt
+++ app/src/main/java/com/example/cubconnect/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.example.cubconnect
 
+import android.app.ActionBar
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmet_container, HomeFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_home)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmet_container, HomeFragment()).commit()
+            R.id.nav_friends -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmet_container, FriendsFragment()).commit()
+            R.id.nav_evets -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmet_container, EventsFragment()).commit()
+            R.id.nav_map -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmet_container, MapFragment()).commit()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }else {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
