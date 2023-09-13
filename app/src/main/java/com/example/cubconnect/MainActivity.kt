4de9c0189186eee5fec073b7958e0173166c4519
Cameron <cameron.obrien@selu.edu>
2023-09-12 22:45:50 -0500

    Map
    Added basic map with pin on SELU

--- app/src/main/java/com/example/cubconnect/MainActivity.kt
+++ app/src/main/java/com/example/cubconnect/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.example.cubconnect
 
-import android.app.ActionBar
-import android.app.Activity
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.MenuItem
-import android.widget.Button
-import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
@@ -49,8 +44,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 .replace(R.id.fragmet_container, FriendsFragment()).commit()
             R.id.nav_evets -> supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmet_container, EventsFragment()).commit()
-            R.id.nav_map -> supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmet_container, MapFragment()).commit()
+            R.id.nav_maps -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmet_container, MapsFragment()).commit()
+            R.id.nav_about -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmet_container, AboutFragment()).commit()
         }
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
