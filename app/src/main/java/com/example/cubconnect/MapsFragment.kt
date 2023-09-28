c64f8f230bf7d38f494909b9c7d191851c14feb3
Cameron <cameron.obrien@selu.edu>
2023-09-28 11:15:39 -0500

    Map is set to SELU
    Mad borders for the map that keep it locked on campus, and made the default view campus.

--- app/src/main/java/com/example/cubconnect/MapsFragment.kt
+++ app/src/main/java/com/example/cubconnect/MapsFragment.kt
@@ -5,34 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), OnMapReadyCallback {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
+    private val Southeastern = LatLng(30.515583, -90.468443)
+    private val ZOOM_LEVEL = 15.5f
 
-        val Southeastern = LatLng(30.51428, 269.53186)
-        googleMap.addMarker(MarkerOptions().position(Southeastern).title("Marker in Southeastern"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(Southeastern))
-
-    }
+    val CampusBounds = LatLngBounds(
+        LatLng((30.508864), -90.471730),  // SW bounds
+        LatLng((30.527527), -90.463864) // NE bounds
+    )
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_maps, container, false)
@@ -40,7 +31,27 @@ class MapsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+
+        val mapFragment: SupportMapFragment? =
+            childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap ?: return
+
+        // Set the LatLngBounds for camera targeting
+        googleMap.setLatLngBoundsForCameraTarget(CampusBounds)
+
+        googleMap ?: return
+        with(googleMap) {
+            moveCamera(
+                com.google.android.gms.maps.CameraUpdateFactory.newLatLngZoom(
+                    Southeastern,
+                    ZOOM_LEVEL
+                )
+            )
+            addMarker(MarkerOptions().position(Southeastern))
+        }
     }
 }
